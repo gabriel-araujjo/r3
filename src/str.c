7bce03cbe3ee73ba16e1303194055cb81bf07c11
Gabriel Araújo <gabriel140492@gmail.com>
2018-07-08 16:12:53 -0300

    Merge of clean cmake

--- src/str.c
+++ src/str.c
@@ -4,269 +4,263 @@
  *
  * Distributed under terms of the MIT license.
  */
+#include "str.h"
 #include "config.h"
+#include "r3.h"
+#include "r3_slug.h"
+#include "slug.h"
+#include "zmalloc.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include "r3.h"
-#include "r3_slug.h"
-#include "str.h"
-#include "slug.h"
-#include "zmalloc.h"
 
-static const char * strnchr(const char* str, unsigned int len, int ch) {
-    for (unsigned int i = 0; i < len; i++) {
-        if (str[i] == ch) return str + i;
+static const char *strnchr(const char *str, unsigned int len, int ch) {
+  for (unsigned int i = 0; i < len; i++) {
+    if (str[i] == ch)
+      return str + i;
+  }
+  return NULL;
+}
+
+int r3_pattern_to_opcode(const char *pattern, unsigned int len) {
+  if (strncmp(pattern, "\\w+", len) == 0) {
+    return OP_EXPECT_MORE_WORDS;
+  }
+  if (strncmp(pattern, "[0-9a-z]+", len) == 0 ||
+      strncmp(pattern, "[a-z0-9]+", len) == 0) {
+    return OP_EXPECT_MORE_WORDS;
+  }
+  if (strncmp(pattern, "[a-z]+", len) == 0) {
+    return OP_EXPECT_MORE_ALPHA;
+  }
+  if (strncmp(pattern, "\\d+", len) == 0) {
+    return OP_EXPECT_MORE_DIGITS;
+  }
+  if (strncmp(pattern, "[0-9]+", len) == 0) {
+    return OP_EXPECT_MORE_DIGITS;
+  }
+  if (strncmp(pattern, "[^/]+", len) == 0) {
+    return OP_EXPECT_NOSLASH;
+  }
+  if (strncmp(pattern, "[^-]+", len) == 0) {
+    return OP_EXPECT_NODASH;
+  }
+  return 0;
+}
+
+char *r3_inside_slug(const char *needle, int needle_len, char *offset,
+                     char **errstr) {
+  char *s1 = offset;
+  char *s2 = offset;
+
+  short found_s1 = 0;
+  short found_s2 = 0;
+
+  while (s1 >= needle && (s1 - needle < needle_len)) {
+    if (*s1 == '{') {
+      found_s1 = 1;
+      break;
+    }
+    s1--;
+  }
+
+  const char *end = needle + needle_len;
+  while ((s2 + 1) < end) {
+    if (*s2 == '}') {
+      found_s2 = 1;
+      break;
+    }
+    s2++;
+  }
+  if (found_s1 && found_s2) {
+    return s1;
+  }
+  if (found_s1 || found_s2) {
+    // wrong slug pattern
+    if (errstr) {
+      asprintf(errstr, "Incomplete slug pattern");
     }
     return NULL;
+  }
+  return NULL;
 }
 
-int r3_pattern_to_opcode(const char * pattern, unsigned int len) {
-    if ( strncmp(pattern, "\\w+",len) == 0 ) {
-        return OP_EXPECT_MORE_WORDS;
+const char *r3_slug_find_placeholder(const char *s1, unsigned int str_len,
+                                     unsigned int *len) {
+  const char *c;
+  const char *s2;
+  int cnt = 0;
+  if ((c = strnchr(s1, str_len, '{'))) {
+    // find closing '}'
+    s2 = c;
+    unsigned int j = str_len - (c - s1);
+    for (unsigned int i = 0; i < j; i++) {
+      if (*s2 == '{')
+        cnt++;
+      else if (*s2 == '}')
+        cnt--;
+      if (cnt == 0)
+        break;
+      s2++;
     }
-    if ( strncmp(pattern, "[0-9a-z]+",len) == 0 ||  strncmp(pattern, "[a-z0-9]+",len) == 0  ) {
-        return OP_EXPECT_MORE_WORDS;
-    }
-    if ( strncmp(pattern, "[a-z]+",len) == 0 ) {
-        return OP_EXPECT_MORE_ALPHA;
-    }
-    if ( strncmp(pattern, "\\d+", len) == 0 ) {
-        return OP_EXPECT_MORE_DIGITS;
-    }
-    if ( strncmp(pattern, "[0-9]+", len) == 0 ) {
-        return OP_EXPECT_MORE_DIGITS;
-    }
-    if ( strncmp(pattern, "[^/]+", len) == 0 ) {
-        return OP_EXPECT_NOSLASH;
-    }
-    if ( strncmp(pattern, "[^-]+", len) == 0 ) {
-        return OP_EXPECT_NODASH;
-    }
-    return 0;
-}
-
-
-
-
-char * r3_inside_slug(const char * needle, int needle_len, char *offset, char **errstr) {
-    char * s1 = offset;
-    char * s2 = offset;
-
-    short found_s1 = 0;
-    short found_s2 = 0;
-
-    while( s1 >= needle && (s1 - needle < needle_len) ) {
-        if ( *s1 == '{' ) {
-            found_s1 = 1;
-            break;
-        }
-        s1--;
-    }
-
-    const char * end = needle + needle_len;
-    while( (s2 + 1) < end ) {
-        if ( *s2 == '}' ) {
-            found_s2 = 1;
-            break;
-        }
-        s2++;
-    }
-    if (found_s1 && found_s2) {
-        return s1;
-    }
-    if (found_s1 || found_s2) {
-        // wrong slug pattern
-        if(errstr) {
-            asprintf(errstr, "Incomplete slug pattern");
-        }
-        return NULL;
-    }
+  } else {
     return NULL;
+  }
+  if (cnt != 0) {
+    return NULL;
+  }
+  if (len) {
+    *len = s2 - c + 1;
+  }
+  return c;
 }
-
-const char * r3_slug_find_placeholder(const char *s1, unsigned int str_len, unsigned int *len) {
-    const char *c;
-    const char *s2;
-    int cnt = 0;
-    if ((c = strnchr(s1, str_len, '{'))) {
-        // find closing '}'
-        s2 = c;
-        unsigned int j = str_len - (c - s1);
-        for (unsigned int i = 0; i < j; i++) {
-            if (*s2 == '{' )
-                cnt++;
-            else if (*s2 == '}' )
-                cnt--;
-            if (cnt == 0)
-                break;
-            s2++;
-        }
-    } else {
-        return NULL;
-    }
-    if (cnt!=0) {
-        return NULL;
-    }
-    if(len) {
-        *len = s2 - c + 1;
-    }
-    return c;
-}
-
 
 /**
  * given a slug string, duplicate the pattern string of the slug
  */
-const char * r3_slug_find_pattern(const char *s1, unsigned int str_len, unsigned int *len) {
-    const char *c;
-    const char *s2;
-    unsigned int cnt = 1;
-    if ( (c = strnchr(s1, str_len, ':')) ) {
-        c++;
-        // find closing '}'
-        s2 = c;
-        unsigned int j = str_len - (c - s1);
-        for (unsigned int i = 0; i < j; i++) {
-            if (*s2 == '{' )
-                cnt++;
-            else if (*s2 == '}' )
-                cnt--;
-            if (cnt == 0)
-                break;
-            s2++;
-        }
+const char *r3_slug_find_pattern(const char *s1, unsigned int str_len,
+                                 unsigned int *len) {
+  const char *c;
+  const char *s2;
+  unsigned int cnt = 1;
+  if ((c = strnchr(s1, str_len, ':'))) {
+    c++;
+    // find closing '}'
+    s2 = c;
+    unsigned int j = str_len - (c - s1);
+    for (unsigned int i = 0; i < j; i++) {
+      if (*s2 == '{')
+        cnt++;
+      else if (*s2 == '}')
+        cnt--;
+      if (cnt == 0)
+        break;
+      s2++;
+    }
 
-    } else {
-        return NULL;
-    }
-    if (cnt!=0) {
-        return NULL;
-    }
-    *len = s2 - c;
-    return c;
+  } else {
+    return NULL;
+  }
+  if (cnt != 0) {
+    return NULL;
+  }
+  *len = s2 - c;
+  return c;
 }
-
 
 /**
  * given a slug string, duplicate the parameter name string of the slug
  */
-const char * r3_slug_find_name(const char *s1, unsigned int str_len, unsigned int *len) {
-    const char * c;
-    const char * s2;
-    unsigned int plholder;
-    if ((c = r3_slug_find_placeholder(s1, str_len, &plholder))) {
-        c++;
-        if (( s2 = strnchr(c, plholder, ':') )) {
-            *len = s2 - c;
-            return c;
-        } else {
-            *len = plholder - 2;
-            return c;
-        }
+const char *r3_slug_find_name(const char *s1, unsigned int str_len,
+                              unsigned int *len) {
+  const char *c;
+  const char *s2;
+  unsigned int plholder;
+  if ((c = r3_slug_find_placeholder(s1, str_len, &plholder))) {
+    c++;
+    if ((s2 = strnchr(c, plholder, ':'))) {
+      *len = s2 - c;
+      return c;
     } else {
-        return NULL;
+      *len = plholder - 2;
+      return c;
     }
+  } else {
+    return NULL;
+  }
 }
-
 
 /**
  * @param char * sep separator
  */
-char * r3_slug_compile(const char * str, unsigned int len)
-{
-    const char *s1 = NULL;
-    char *o = NULL;
-    const char *pat = NULL;
-    char sep = '/';
+char *r3_slug_compile(const char *str, unsigned int len) {
+  const char *s1 = NULL;
+  char *o = NULL;
+  const char *pat = NULL;
+  char sep = '/';
 
+  // append prefix
+  unsigned int s1_len;
+  s1 = r3_slug_find_placeholder(str, len, &s1_len);
 
-    // append prefix
-    unsigned int s1_len;
-    s1 = r3_slug_find_placeholder(str, len, &s1_len);
+  if (!s1) {
+    return zstrndup(str, len);
+  }
 
-    if ( !s1 ) {
-        return zstrndup(str,len);
-    }
+  char *out = NULL;
+  if (!(out = zcalloc(sizeof(char) * 200))) {
+    return (NULL);
+  }
 
-    char * out = NULL;
-    if (!(out = zcalloc(sizeof(char) * 200))) {
-        return (NULL);
-    }
+  o = out;
+  *o = '^';
+  o++;
 
-    o = out;
-    strncat(o, "^", 1);
+  memcpy(o, str, s1 - str); // string before slug
+  o += (s1 - str);
+
+  unsigned int pat_len;
+  pat = r3_slug_find_pattern(s1, s1_len, &pat_len);
+
+  if (pat) {
+    *o = '(';
     o++;
-
-    strncat(o, str, s1 - str); // string before slug
-    o += (s1 - str);
-
-
-    unsigned int pat_len;
-    pat = r3_slug_find_pattern(s1, s1_len, &pat_len);
-
-    if (pat) {
-        *o = '(';
-        o++;
-        strncat(o, pat, pat_len );
-        o += pat_len;
-        *o = ')';
-        o++;
-    } else {
-        sprintf(o, "([^%c]+)", sep);
-        o+= strlen("([^*]+)");
-    }
-    s1 += s1_len;
-    strncat(o, s1, len - (s1 - str)); // string after slug
-    return out;
+    memcpy(o, pat, pat_len);
+    o += pat_len;
+    *o = ')';
+    o++;
+  } else {
+    sprintf(o, "([^%c]+)", sep);
+    o += strlen("([^*]+)");
+  }
+  s1 += s1_len;
+  memcpy(o, s1, len - (s1 - str)); // string after slug
+  o += len - (s1 - str);
+  *o = '\0';
+  return out;
 }
 
-
-char * ltrim_slash(char* str)
-{
-    char * p = str;
-    while (*p == '/') p++;
-    return zstrdup(p);
+char *ltrim_slash(char *str) {
+  char *p = str;
+  while (*p == '/')
+    p++;
+  return zstrdup(p);
 }
 
 void print_indent(int level) {
-    int len = level * 2;
-    while(len--) {
-        printf(" ");
-    }
+  int len = level * 2;
+  while (len--) {
+    printf(" ");
+  }
 }
-
-
 
 #ifndef HAVE_STRDUP
 char *zstrdup(const char *s) {
-    char *out;
-    int count = 0;
-    while( s[count] )
-        ++count;
+  char *out;
+  int count = 0;
+  while (s[count])
     ++count;
-    out = zmalloc(sizeof(char) * count);
-    out[--count] = 0;
-    while( --count >= 0 )
-        out[count] = s[count];
-    return out;
+  ++count;
+  out = zmalloc(sizeof(char) * count);
+  out[--count] = 0;
+  while (--count >= 0)
+    out[count] = s[count];
+  return out;
 }
 #endif
 
-
-
 #ifndef HAVE_STRNDUP
 char *zstrndup(const char *s, int n) {
-    char *out;
-    int count = 0;
-    while( count < n && s[count] )
-        ++count;
+  char *out;
+  int count = 0;
+  while (count < n && s[count])
     ++count;
-    out = zmalloc(sizeof(char) * count);
-    out[--count] = 0;
-    while( --count >= 0 )
-        out[count] = s[count];
-    return out;
+  ++count;
+  out = zmalloc(sizeof(char) * count);
+  out[--count] = 0;
+  while (--count >= 0)
+    out[count] = s[count];
+  return out;
 }
 #endif
--- tests/check_slug.c
+++ tests/check_slug.c
@@ -5,126 +5,119 @@
  * Distributed under terms of the MIT license.
  */
 #include "config.h"
-#include <stdio.h>
-#include <check.h>
-#include <stdlib.h>
 #include "r3.h"
-#include "r3_slug.h"
-#include "zmalloc.h"
-#include "slug.h"
 #include "r3_debug.h"
+#include "r3_slug.h"
+#include "slug.h"
+#include "zmalloc.h"
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-START_TEST (test_pattern_to_opcode)
-{
-    ck_assert( r3_pattern_to_opcode("\\w+", strlen("\\w+")) == OP_EXPECT_MORE_WORDS );
-    ck_assert( r3_pattern_to_opcode("\\d+", strlen("\\d+")) == OP_EXPECT_MORE_DIGITS );
-    ck_assert( r3_pattern_to_opcode("[^/]+",strlen("[^/]+")) == OP_EXPECT_NOSLASH );
-    ck_assert( r3_pattern_to_opcode("[^-]+",strlen("[^-]+")) == OP_EXPECT_NODASH );
+START_TEST(test_pattern_to_opcode) {
+  ck_assert(r3_pattern_to_opcode("\\w+", strlen("\\w+")) ==
+            OP_EXPECT_MORE_WORDS);
+  ck_assert(r3_pattern_to_opcode("\\d+", strlen("\\d+")) ==
+            OP_EXPECT_MORE_DIGITS);
+  ck_assert(r3_pattern_to_opcode("[^/]+", strlen("[^/]+")) ==
+            OP_EXPECT_NOSLASH);
+  ck_assert(r3_pattern_to_opcode("[^-]+", strlen("[^-]+")) == OP_EXPECT_NODASH);
 }
 END_TEST
 
-START_TEST (test_r3_slug_compile)
-{
-    char * path = "/user/{id}";
-    char * c = NULL;
-    ck_assert_str_eq( c = r3_slug_compile(path, strlen(path) ) , "^/user/([^/]+)" );
-    zfree(c);
+START_TEST(test_r3_slug_compile) {
+  char *path = "/user/{id}";
+  char *c = NULL;
+  ck_assert_str_eq(c = r3_slug_compile(path, strlen(path)), "^/user/([^/]+)");
+  zfree(c);
 
-    char * path2 = "/what/{id}-foo";
-    ck_assert_str_eq( c = r3_slug_compile(path2, strlen(path2) ) , "^/what/([^/]+)-foo" );
-    zfree(c);
+  char *path2 = "/what/{id}-foo";
+  ck_assert_str_eq(c = r3_slug_compile(path2, strlen(path2)),
+                   "^/what/([^/]+)-foo");
+  zfree(c);
 
-    char * path3 = "-{id}";
-    ck_assert_str_eq( c = r3_slug_compile(path3, strlen(path3)), "^-([^/]+)" );
-    zfree(c);
+  char *path3 = "-{id}";
+  ck_assert_str_eq(c = r3_slug_compile(path3, strlen(path3)), "^-([^/]+)");
+  zfree(c);
 
-    char * path4 = "-{idx:\\d{3}}";
-    ck_assert_str_eq( c = r3_slug_compile(path4, strlen(path4)), "^-(\\d{3})" );
-    zfree(c);
+  char *path4 = "-{idx:\\d{3}}";
+  ck_assert_str_eq(c = r3_slug_compile(path4, strlen(path4)), "^-(\\d{3})");
+  zfree(c);
 }
 END_TEST
 
-START_TEST (test_contains_slug)
-{
-    char *test_str = "/user/{id}/{name}";
-    ck_assert( r3_path_contains_slug_char(test_str, strlen(test_str)) );
+START_TEST(test_contains_slug) {
+  char *test_str = "/user/{id}/{name}";
+  ck_assert(r3_path_contains_slug_char(test_str, strlen(test_str)));
 }
 END_TEST
 
-START_TEST (test_r3_slug_find_pattern)
-{
-    int len;
-    char *test_str = "{name:\\s+}";
-    char * namerex = r3_slug_find_pattern(test_str, strlen(test_str), &len);
-    ck_assert( strncmp(namerex, "\\s+", len) == 0 );
+START_TEST(test_r3_slug_find_pattern) {
+  unsigned int len;
+  char *test_str = "{name:\\s+}";
+  const char *namerex = r3_slug_find_pattern(test_str, strlen(test_str), &len);
+  ck_assert(strncmp(namerex, "\\s+", len) == 0);
 }
 END_TEST
 
-START_TEST (test_r3_slug_find_name)
-{
-    int len;
-    char *test_str = "{name:\\s+}";
-    char * namerex = r3_slug_find_name(test_str, strlen(test_str), &len);
-    ck_assert( strncmp(namerex, "name", len) == 0 );
+START_TEST(test_r3_slug_find_name) {
+  unsigned int len;
+  char *test_str = "{name:\\s+}";
+  const char *namerex = r3_slug_find_name(test_str, strlen(test_str), &len);
+  ck_assert(strncmp(namerex, "name", len) == 0);
 }
 END_TEST
 
-START_TEST (test_r3_slug_find_name_without_pattern)
-{
-    int len;
-    char *test_str = "{name}";
-    char * namerex = r3_slug_find_name(test_str, strlen(test_str), &len);
-    ck_assert( strncmp(namerex, "name", len) == 0 );
+START_TEST(test_r3_slug_find_name_without_pattern) {
+  unsigned int len;
+  char *test_str = "{name}";
+  const char *namerex = r3_slug_find_name(test_str, strlen(test_str), &len);
+  ck_assert(strncmp(namerex, "name", len) == 0);
 }
 END_TEST
 
-START_TEST (test_r3_slug_find_name_with_multiple_slug)
-{
-    int len;
-    char *test_str = "{name}/{name2}";
-    char * namerex = r3_slug_find_name(test_str, strlen(test_str), &len);
-    ck_assert( strncmp(namerex, "name", len) == 0 );
+START_TEST(test_r3_slug_find_name_with_multiple_slug) {
+  unsigned int len;
+  char *test_str = "{name}/{name2}";
+  const char *namerex = r3_slug_find_name(test_str, strlen(test_str), &len);
+  ck_assert(strncmp(namerex, "name", len) == 0);
 }
 END_TEST
 
-START_TEST (test_r3_slug_find_placeholder)
-{
-    int slug_len = 0;
-    char * slug;
-    char *test_str = "/user/{name:\\s+}/to/{id}";
-    slug = r3_slug_find_placeholder(test_str, strlen(test_str), &slug_len);
-    ck_assert( strncmp(slug, "{name:\\s+}", slug_len) == 0 );
+START_TEST(test_r3_slug_find_placeholder) {
+  unsigned int slug_len = 0;
+  const char *slug;
+  char *test_str = "/user/{name:\\s+}/to/{id}";
+  slug = r3_slug_find_placeholder(test_str, strlen(test_str), &slug_len);
+  ck_assert(strncmp(slug, "{name:\\s+}", slug_len) == 0);
 
-    test_str = "/user/{idx:\\d{3}}/to/{idy:\\d{3}}";
-    slug = r3_slug_find_placeholder(test_str, strlen(test_str), &slug_len);
-    ck_assert( slug_len == strlen("{idx:\\d{3}}") );
-    ck_assert( strncmp(slug, "{idx:\\d{3}}", slug_len) == 0 );
+  test_str = "/user/{idx:\\d{3}}/to/{idy:\\d{3}}";
+  slug = r3_slug_find_placeholder(test_str, strlen(test_str), &slug_len);
+  ck_assert(slug_len == strlen("{idx:\\d{3}}"));
+  ck_assert(strncmp(slug, "{idx:\\d{3}}", slug_len) == 0);
 }
 END_TEST
 
-START_TEST (test_r3_inside_slug)
-{
-    char * pattern = "/user/{name:\\s+}/to/{id}";
-    char * offset = strchr(pattern, '{') + 2;
-    ck_assert( r3_inside_slug(pattern, strlen(pattern), offset, NULL) != NULL );
-    ck_assert( *(r3_inside_slug(pattern, strlen(pattern), offset, NULL)) == '{' );
-    ck_assert( ! r3_inside_slug(pattern, strlen(pattern), pattern, NULL) );
+START_TEST(test_r3_inside_slug) {
+  char *pattern = "/user/{name:\\s+}/to/{id}";
+  char *offset = strchr(pattern, '{') + 2;
+  ck_assert(r3_inside_slug(pattern, strlen(pattern), offset, NULL) != NULL);
+  ck_assert(*(r3_inside_slug(pattern, strlen(pattern), offset, NULL)) == '{');
+  ck_assert(!r3_inside_slug(pattern, strlen(pattern), pattern, NULL));
 }
 END_TEST
 
-START_TEST (test_incomplete_slug)
-{
-    int cnt = 0;
-    char * errstr = NULL;
-    char * pattern = "/user/{name:\\d{3}}/to/{id";
-    cnt = r3_slug_count(pattern, strlen(pattern), &errstr);
-    ck_assert_int_eq(cnt, -1);
-    ck_assert(errstr);
-    printf("%s\n",errstr);
-    free(errstr);
+START_TEST(test_incomplete_slug) {
+  int cnt = 0;
+  char *errstr = NULL;
+  char *pattern = "/user/{name:\\d{3}}/to/{id";
+  cnt = r3_slug_count(pattern, strlen(pattern), &errstr);
+  ck_assert_int_eq(cnt, -1);
+  ck_assert(errstr);
+  printf("%s\n", errstr);
+  free(errstr);
 }
 END_TEST
-
 
 /*
 START_TEST (test_slug_parse_with_pattern)
@@ -163,72 +156,68 @@ START_TEST (test_slug_parse_without_pattern)
 END_TEST
 */
 
+START_TEST(test_r3_slug_count) {
+  int cnt = 0;
+  char *pattern = "/user/{name:\\s+}/to/{id}";
+  char *errstr = NULL;
+  cnt = r3_slug_count(pattern, strlen(pattern), &errstr);
+  ck_assert_int_eq(cnt, 2);
+  if (errstr)
+    free(errstr);
 
+  char *pattern2 = "/user/{name:\\d{3}}/to/{id}";
+  cnt = r3_slug_count(pattern2, strlen(pattern2), &errstr);
+  ck_assert_int_eq(cnt, 2);
+  if (errstr)
+    free(errstr);
 
-
-
-
-START_TEST (test_r3_slug_count)
-{
-    int cnt = 0;
-    char * pattern = "/user/{name:\\s+}/to/{id}";
-    char * errstr = NULL;
-    cnt = r3_slug_count(pattern, strlen(pattern), &errstr);
-    ck_assert_int_eq(cnt, 2);
-    if(errstr) free(errstr);
-
-    char * pattern2 = "/user/{name:\\d{3}}/to/{id}";
-    cnt = r3_slug_count(pattern2, strlen(pattern2), &errstr);
-    ck_assert_int_eq(cnt, 2);
-    if(errstr) free(errstr);
-
-    char * pattern3 = "/user/{name:\\d{3}}/to/{id}";
-    cnt = r3_slug_count(pattern3, strlen(pattern3), &errstr);
-    ck_assert_int_eq(cnt, 2);
-    if(errstr) free(errstr);
+  char *pattern3 = "/user/{name:\\d{3}}/to/{id}";
+  cnt = r3_slug_count(pattern3, strlen(pattern3), &errstr);
+  ck_assert_int_eq(cnt, 2);
+  if (errstr)
+    free(errstr);
 }
 END_TEST
 
-START_TEST (test_r3_slug_find_placeholder_with_broken_slug)
-{
-    int slug_len = 0;
-    char *sl_test = "/user/{name:\\s+/to/{id";
-    char * slug = r3_slug_find_placeholder(sl_test, strlen(sl_test), &slug_len);
-    ck_assert(slug == 0);
+START_TEST(test_r3_slug_find_placeholder_with_broken_slug) {
+  unsigned int slug_len = 0;
+  char *sl_test = "/user/{name:\\s+/to/{id";
+  const char *slug =
+      r3_slug_find_placeholder(sl_test, strlen(sl_test), &slug_len);
+  ck_assert(slug == 0);
 }
 END_TEST
 
+Suite *r3_suite(void) {
+  Suite *suite = suite_create("slug test");
+  TCase *tcase = tcase_create("test_slug");
+  tcase_set_timeout(tcase, 30);
+  tcase_add_test(tcase, test_contains_slug);
+  tcase_add_test(tcase, test_r3_inside_slug);
+  tcase_add_test(tcase, test_r3_slug_find_pattern);
+  tcase_add_test(tcase, test_r3_slug_find_placeholder);
+  tcase_add_test(tcase, test_r3_slug_find_placeholder_with_broken_slug);
+  tcase_add_test(tcase, test_r3_slug_count);
+  tcase_add_test(tcase, test_r3_slug_compile);
+  tcase_add_test(tcase, test_pattern_to_opcode);
+  tcase_add_test(tcase, test_incomplete_slug);
+  tcase_add_test(tcase, test_r3_slug_find_name);
+  tcase_add_test(tcase, test_r3_slug_find_name_without_pattern);
+  tcase_add_test(tcase, test_r3_slug_find_name_with_multiple_slug);
 
-Suite* r3_suite (void) {
-        Suite *suite = suite_create("slug test");
-        TCase *tcase = tcase_create("test_slug");
-        tcase_set_timeout(tcase, 30);
-        tcase_add_test(tcase, test_contains_slug);
-        tcase_add_test(tcase, test_r3_inside_slug);
-        tcase_add_test(tcase, test_r3_slug_find_pattern);
-        tcase_add_test(tcase, test_r3_slug_find_placeholder);
-        tcase_add_test(tcase, test_r3_slug_find_placeholder_with_broken_slug);
-        tcase_add_test(tcase, test_r3_slug_count);
-        tcase_add_test(tcase, test_r3_slug_compile);
-        tcase_add_test(tcase, test_pattern_to_opcode);
-        tcase_add_test(tcase, test_incomplete_slug);
-        tcase_add_test(tcase, test_r3_slug_find_name);
-        tcase_add_test(tcase, test_r3_slug_find_name_without_pattern);
-        tcase_add_test(tcase, test_r3_slug_find_name_with_multiple_slug);
-        
-        // tcase_add_test(tcase, test_slug_parse_with_pattern);
-        // tcase_add_test(tcase, test_slug_parse_without_pattern);
+  // tcase_add_test(tcase, test_slug_parse_with_pattern);
+  // tcase_add_test(tcase, test_slug_parse_without_pattern);
 
-        suite_add_tcase(suite, tcase);
-        return suite;
+  suite_add_tcase(suite, tcase);
+  return suite;
 }
 
-int main (int argc, char *argv[]) {
-        int number_failed;
-        Suite *suite = r3_suite();
-        SRunner *runner = srunner_create(suite);
-        srunner_run_all(runner, CK_NORMAL);
-        number_failed = srunner_ntests_failed(runner);
-        srunner_free(runner);
-        return number_failed;
+int main(int argc, char *argv[]) {
+  int number_failed;
+  Suite *suite = r3_suite();
+  SRunner *runner = srunner_create(suite);
+  srunner_run_all(runner, CK_NORMAL);
+  number_failed = srunner_ntests_failed(runner);
+  srunner_free(runner);
+  return number_failed;
 }
